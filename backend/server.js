dce3d277acd10d100f424ff769c157889c6ba485
Mouhammad ndour <ndourmouhammad15@gmail.com>
2025-02-13 11:20:15 +0000

    MongoDB Setup and the creation of the Product Model

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";    
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Server already running");
+app.get("/products", (req, res) => {
+    
 });
 
 app.listen(5000, () => {
-    console.log("Server is running on port 5000");
+    connectDB();
+    console.log("Server started on port 5000");
 });
